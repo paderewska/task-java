cf0e9232e517116d0c7a3b8b177962e21e2c1bce
paderewska <iza.paderewska@gmail.com>
2018-03-12 20:39:10 +0100

    18.2 Poprawki

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -21,7 +21,7 @@ public class TrelloClient {
     @Value("${trello.app.token}")
     private String trelloToken;
 
-    @Value("${trello.app.username")
+    @Value("${trello.app.username}")
     private String trelloUsername;
 
     @Autowired
@@ -29,7 +29,7 @@ public class TrelloClient {
 
     private URI buildUrl() {
 
-        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/programistkaiza/boards")
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
                 .queryParam("fields", "name,id").build().encode().toUri();
