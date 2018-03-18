4b42f1310b24c77a3421489ae3e93fbaffee4650
paderewska <iza.paderewska@gmail.com>
2018-03-18 15:30:36 +0100

    19.1 Poprawki

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -15,13 +15,14 @@ public class CreatedTrelloCard {
     @JsonProperty("id")
     private String id;
 
-//    @JsonProperty("badges")
-//    private Badges badges;
-
     @JsonProperty("name")
     private String name;
 
     @JsonProperty("shortUrl")
     private String shortUrl;
+
+    @JsonProperty("badges")
+    private Badges badges;
+
 
 }
